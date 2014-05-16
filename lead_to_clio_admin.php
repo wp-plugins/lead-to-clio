fe12db7f599265ba6387ac3133a27ffa2e88852a
tripgrass <tripgrass@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-16 15:43:50 +0000

    Commit Version 1
    git-svn-id: https://plugins.svn.wordpress.org/lead-to-clio/trunk@915673 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ lead_to_clio_admin.php
@@ -0,0 +1,72 @@
+<?php 
+	$referer = $_SERVER['HTTP_HOST'];
+	$script = $_SERVER['SCRIPT_NAME'];
+	$redirect = "http://".$referer.$script;
+	$clio_source = $_SERVER['SERVER_NAME'];
+	$admin_page = $_SERVER['SCRIPT_NAME'];
+	if($clio_auth = $_GET['clio_auth']) {
+		update_option('clio_source', $clio_source);
+		update_option('casewave_account_id', $_GET['casewave_account_id']);
+		echo "<div class='updated'><p><strong>";
+		_e('Options saved.' );
+		echo"</strong></p></div>";
+	}
+	else {
+		$casewave_account_id = get_option('casewave_account_id');
+	}
+	if($_GET['casewave_account_id']){
+		$casewave_account_id = $_GET['casewave_account_id'];
+	}
+	if($casewave_account_id){
+		$httpGet = 'http://www.casewave.com/include/Submit/submitCasewaveCheck.php?casewave_account_id='.$casewave_account_id;
+		$casewaveCheckArray = wp_remote_get($httpGet);
+		if($casewaveCheckArray['body'] != 'is_valid'){
+			unset($clio_auth);
+		}
+		else{
+			$clio_auth = true;
+		}
+	}
+	if($_POST['clear_clio']=='Y'){
+		delete_option('clio_source');
+	}
+?>
+		<div class="wrap">
+			<h2>Lead-to-Clio Options</h2>
+<?php 
+	$state_array = array(
+		"redirect"=> $redirect,
+		"group_id"=>$casewave_account_id,
+		"clio_source"=>$clio_source,
+		"admin_page"=>$admin_page
+	);
+	$state_array = json_encode($state_array);
+	$url = "https://app.goclio.com/oauth/authorize";
+	$params = array(
+		"response_type" => "code",
+		"client_id" => "HwhWVxxMgPHDR1E6T2GGHh24GskDCN15OhnqzOWP",
+		"redirect_uri" => "http://www.casewave.com/include/Submit/submitClioClientAuth.php",
+		"state" => $state_array
+  	);
+	$request_to = $url . '?' . http_build_query($params);
+	if(!$clio_auth || $_POST['clear_clio']=='Y'){
+	?>
+				<form name="lead_to_clio_form" method="post" action="<?php echo str_replace( '%7E', '~', $_SERVER['REQUEST_URI']); ?>">
+				<input type="hidden" name="lead_to_clio_hidden" value="Y">
+<?php
+		_e("<a href='".$request_to."'>Authorize CaseWave's Lead-to-Clio to Add Contacts to Clio</a>");
+	}
+	else{
+	?>
+				<form name="clear_clio" method="post" action="<?php echo str_replace( '%7E', '~', $_SERVER['REQUEST_URI']); ?>">
+				<input type="hidden" name="clear_clio" value="Y">
+<?php
+		_e("You Have authorized CaseWave's Lead-to-Clio to Add Contacts to Clio");
+		echo "<br><button>Delete Authorization</button>";	
+	}
+?>
+				<!--<p class="submit">
+				<input type="submit" name="Submit" value="<?php _e('Update Options', 'oscimp_trdom' ) ?>" />
+				</p>-->
+			</form>
+		</div>
---
+++ readme.txt
@@ -0,0 +1,26 @@
+=== Plugin Name ===
+Contributors: tripgrass
+Tags: form, Clio
+Requires at least: 3.0.1
+Tested up to: 3.4
+Stable tag: 4.3
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Lead-to-Clio integrates your Wordpress Blog with your Clio Account - automatically creating tasks and contacts for new leads.
+
+== Description ==
+
+Lead-to-Clio provides a contact form of first name, last name, phone number and email that integrates with the Clio Online Legal Practice Management Software. If the user has authorized Lead-to-Clio to access the user's Clio account, when a prospective client submits their contact info on the form a new Contact entry is created in Clio, a new task entry is created in Clio and a notification email is sent to the Clio users.
+A few notes about the sections above:
+
+== Installation ==
+
+1. Upload `Lead-to-Clio` folder to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. On the 'Lead-to-Clio' Settings page (found from your Wordpress dashboard) - click on the "Authorize Clio" link. 
+
+== Changelog ==
+
+= 1.0 = Version 1
+
